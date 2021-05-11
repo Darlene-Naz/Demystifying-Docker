368ae492fc9a09b9c12d8e30cef456891361289b
Darlene Nazareth <46684660+Darlene-Naz@users.noreply.github.com>
2021-05-11 16:53:57 +0530

    :+1: displayed server id, port

--- Load-Balanced-Nodejs-App/app/index.js
+++ Load-Balanced-Nodejs-App/app/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000; // For testing locally
@@ -8,8 +9,10 @@ const SERVER_ID = process.env.SERVER_ID || 2; // For testing locally
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.get("/", (req, res) => {
-    console.log({ server: SERVER_ID, port: PORT })
-    res.sendFile(path.join(__dirname, "/static/index.html"));
+    console.log({ server: SERVER_ID, port: PORT });
+    let file = fs.readFileSync(path.join(__dirname, "/static/index.html"), 'utf8');
+    file = file.replace('Darlene', '{ SERVER_ID: ' + SERVER_ID + ', PORT: ' + PORT + ' }');
+    res.send(file);
 });
 
 app.listen(PORT, function() {
